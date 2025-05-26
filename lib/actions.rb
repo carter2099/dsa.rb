667b951178704d51fa68c286ceff4d071068f67c
carter2099 <carter2077@pm.me>
2025-05-26 14:15:42 -0400

    refactor how tests are gathered and loaded

--- lib/actions.rb
+++ lib/actions.rb
@@ -1,20 +1,12 @@
 module Actions
   def self.run_tests
     puts "\n"
-    load_and_gather.each do
+    load_tests
+    list_tests.each do
       # load_and_gather requires the files making the test methods callable locally
       send _1
       puts "\n"
     end
-  end
-
-  private_class_method def self.load_and_gather
-    tests = []
-    Dir['./lib/tests/*.rb'].each do |file|
-      require file
-      tests << File.basename(file, '.rb')
-    end
-    tests
   end
 
   def self.init
@@ -27,5 +19,23 @@ module Actions
 
   def self.restore
     puts 'restore'
+  end
+
+  private_class_method def self.list_test_files
+    Dir['./lib/tests/*.rb']
+  end
+
+  private_class_method def self.list_tests
+    list_test_files.map { File.basename _1, '.rb' }
+  end
+
+  private_class_method def self.load_tests
+    list_test_files.each { require _1 }
+  end
+
+  private_class_method def self.load_and_gather
+  end
+
+  private_class_method def self.create_imp_files
   end
 end
