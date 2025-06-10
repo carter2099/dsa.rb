ae475b632b3dc3cd232f882cf59f962081386636
carter2099 <carter2077@pm.me>
2025-06-10 09:17:39 -0400

    remove sliding window

--- lib/tests/test_sliding_window.rb
+++
@@ -1,9 +0,0 @@
-def test_sliding_window
-  puts '- Sliding Window -'
-  require_relative('../../imps/sliding_window')
-  # tests...
-rescue LoadError
-  puts 'LoadError: imps/sliding_window.rb not found'
-rescue StandardError
-  puts 'Error'
-end
