059e6415868a05faeb3b347624c5eb84d5a1ca8a
carter2099 <carter2077@pm.me>
2025-06-10 10:05:25 -0400

    binary search tests

--- lib/tests/test_binary_search.rb
+++ lib/tests/test_binary_search.rb
@@ -1,9 +1,21 @@
 def test_binary_search
   puts '- Binary Search -'
   require_relative('../../imps/binary_search')
-  # tests...
+
+  arr = TestHelpers.create_arr
+
+  puts 'Right middle'
+  TestHelpers.run_test { binary_search(arr, 9) == arr.index(9) }
+  puts 'Left middle'
+  TestHelpers.run_test { binary_search(arr, -2) == arr.index(-2) }
+  puts('First index')
+  TestHelpers.run_test { binary_search(arr, -12) == arr.index(-12) }
+  puts('Last index')
+  TestHelpers.run_test { binary_search(arr, 99) == arr.index(99) }
+  puts('Not found')
+  TestHelpers.run_test { binary_search(arr, 4).nil? }
 rescue LoadError
   puts 'LoadError: imps/binary_search.rb not found'
-rescue StandardError
-  puts 'Error'
+rescue StandardError => e
+  puts "Error: #{e}"
 end
