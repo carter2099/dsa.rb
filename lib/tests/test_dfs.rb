ae8fe73a0b1b2dac579fb03decea58e477f5999c
carter2099 <carter2077@pm.me>
2025-06-10 10:00:11 -0400

    dfs tests

--- lib/tests/test_dfs.rb
+++ lib/tests/test_dfs.rb
@@ -1,7 +1,24 @@
+require_relative('../test_helpers/test_helpers')
 def test_dfs
   puts '- Depth First Search -'
   require_relative('../../imps/dfs')
-  # tests...
+
+  graph = TestHelpers.create_graph
+
+  puts 'Node somewhere in the middle'
+  TestHelpers.run_test { dfs(graph, 10).value == 10 }
+  puts 'Left leaf'
+  TestHelpers.run_test { dfs(graph, 7).value == 7 }
+  puts 'Right leaf'
+  TestHelpers.run_test { dfs(graph, 3).value == 3 }
+  puts 'Start from right leaf find left leaf'
+  TestHelpers.run_test { dfs(graph, 7).value == 7 }
+  puts 'Start from left leaf find right leaf'
+  TestHelpers.run_test { dfs(graph, 3).value == 3 }
+  puts 'Find self'
+  TestHelpers.run_test { dfs(graph, 1).value == 1 }
+  puts 'Not found'
+  TestHelpers.run_test { dfs(graph, 99).nil? }
 rescue LoadError
   puts 'LoadError imps/dfs.rb not found'
 rescue StandardError
