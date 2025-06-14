62d492295a133db7a9270ee91c4244cbcc29e308
carter2099 <carter2077@pm.me>
2025-06-14 14:24:33 -0400

    update test template

--- test_template.rb
+++ test_template.rb
@@ -1,9 +1,13 @@
-def test_template
-  puts '- template -'
-  require_relative('../../imps/template')
-  # tests...
-rescue LoadError
-  puts 'LoadError: imps/template.rb not found'
-rescue StandardError
-  puts 'Error'
+require('minitest/autorun')
+
+class TestTemplate < Minitest::Test
+  def setup
+    require_relative('../../imps/template')
+    # other setup...
+  rescue LoadError
+    puts 'LoadError: imps/template.rb not found'
+  end
+
+  def test_something
+  end
 end
