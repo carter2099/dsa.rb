538fefd42872e22630cf954214cf6640844f2370
carter2099 <carter2077@pm.me>
2025-06-14 13:45:53 -0400

    dfs use minitest

--- lib/tests/test_dfs.rb
+++ lib/tests/test_dfs.rb
@@ -1,26 +1,42 @@
 require_relative('../test_helpers/test_helpers')
-def test_dfs
-  puts '- Depth First Search -'
-  require_relative('../../imps/dfs')
+require('minitest/autorun')
 
-  graph = TestHelpers.create_graph
+class TestDfs < Minitest::Test
+  def setup
+    require_relative('../../imps/dfs')
+    @graph_hash = TestHelpers.create_graph
+  rescue LoadError
+    puts 'LoadError: imps/dfs.rb not found'
+  end
 
-  puts 'Node somewhere in the middle'
-  TestHelpers.run_test { dfs(graph, 10).value == 10 }
-  puts 'Left leaf'
-  TestHelpers.run_test { dfs(graph, 7).value == 7 }
-  puts 'Right leaf'
-  TestHelpers.run_test { dfs(graph, 3).value == 3 }
-  puts 'Start from right leaf find left leaf'
-  TestHelpers.run_test { dfs(graph, 7).value == 7 }
-  puts 'Start from left leaf find right leaf'
-  TestHelpers.run_test { dfs(graph, 3).value == 3 }
-  puts 'Find self'
-  TestHelpers.run_test { dfs(graph, 1).value == 1 }
-  puts 'Not found'
-  TestHelpers.run_test { dfs(graph, 99).nil? }
-rescue LoadError
-  puts 'LoadError imps/dfs.rb not found'
-rescue StandardError => e
-  puts "Error: #{e}"
+  def assert_dfs_value(graph, target, expected = target)
+    if expected.nil?
+      assert_nil(dfs(graph, target))
+    else
+      assert_equal(dfs(graph, target)&.value, expected)
+    end
+  end
+
+  def assert_all_nodes(graph)
+    (0..12).each { assert_dfs_value graph, _1 }
+    assert_dfs_value(graph, 50, nil)
+    assert_dfs_value(graph, 99, nil)
+    assert_dfs_value(graph, -50, nil)
+    assert_dfs_value(graph, -99, nil)
+  end
+
+  def test_from_middle
+    puts "\nDFS find nodes from the middle node"
+    assert_all_nodes @graph_hash[1]
+  end
+
+  def test_from_right_leaf
+    puts "\nDFS find nodes from the right leaf"
+    assert_all_nodes @graph_hash[11]
+  end
+
+  def test_from_left_leaf
+    puts "\nDFS find nodes from the left leaf"
+    assert_all_nodes @graph_hash[8]
+  end
 end
