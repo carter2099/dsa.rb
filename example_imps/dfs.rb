b2851a13dff69864e2a8e0f5feb35f4c270fb3d6
carter2099 <carter2077@pm.me>
2025-06-19 10:21:03 -0400

    update dfs example

--- example_imps/dfs.rb
+++ example_imps/dfs.rb
@@ -8,7 +8,7 @@ def dfs_helper(node, target, visited)
   visited << node
 
   node.neighbors.filter { !visited.include? _1 }.each do |neighbor|
-    res = dfs_helper neighbor, target, visited
+    res = dfs_helper(neighbor, target, visited)
     return res if res
   end
   nil
