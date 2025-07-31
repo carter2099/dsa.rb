c7337dd0b7a6daf90ee285553dc20456c86575fb
carter2099 <carter2077@pm.me>
2025-07-30 22:01:05 -0400

    update test template

--- test_template.rb
+++ test_template.rb
@@ -1,3 +1,4 @@
+require_relative('../test_helpers/test_helpers')
 require('minitest/autorun')
 
 class TestTemplate < Minitest::Test
@@ -5,7 +6,18 @@ class TestTemplate < Minitest::Test
     require_relative('../../imps/template')
     # other setup...
   rescue LoadError
-    puts 'LoadError: imps/template.rb not found'
+    puts
+    puts 'LoadError: imps/template.rb not found' unless @no_file
+    @no_file = true
+  end
+
+  def assert_template_value
+    # assert things... see examples in lib/tests/
+  rescue NoMethodError => e
+    raise e unless e.message.match?(/undefined method 'template'/)
+
+    puts '  ...Skipping test, Template not implemented...' unless @no_imp
+    @no_imp = true
   end
 
   def test_something
